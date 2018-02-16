0f92477b3ebc65a22c2590e09f13ef4de7993420
SardineFish <Sardine@live.cn>
2018-02-16 19:57:14 +0800

    use EBO & render rect

--- FirstOpenGL/src/main.cpp
+++ FirstOpenGL/src/main.cpp
@@ -17,7 +17,7 @@ void initShader();
 void initBuffer();
 
 unsigned int shaderProgram = 0;
-unsigned int VBO, VAO;
+unsigned int VBO, VAO, EBO;
 
 int main()
 {
@@ -86,9 +86,10 @@ void render()
 	glClearColor(RGBA(50, 50, 50, 1.0));
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glUseProgram(shaderProgram);
-	glBindVertexArray(VAO);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glUseProgram(shaderProgram); 
+	glBindVertexArray(VAO); 
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 }
 
 void initShader()
@@ -116,20 +117,33 @@ void initShader()
 void initBuffer()
 {
 	float vertices[] = {
-		-.5f, -.5f, 0.0f,
-		.5f, -.5f, 0.0f,
-		0.0f, .5f, 0.0f,
+		.5f,.5f,0,
+		.5f,-.5f,0,
+		-.5f,-.5f,0,
+		-.5f,.5f,0
+	};
+	unsigned int indices[] = {
+		0,1,3,
+		1,2,3
 	};
 
 	glGenBuffers(1, &VBO);
 	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &EBO);
+
+	// Bind VAO
 	glBindVertexArray(VAO);
 
+	// Copy vertices into VBO (?
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	// Copy index array into EBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Set the vertex attributes pointers
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glBindVertexArray(0);
 }
